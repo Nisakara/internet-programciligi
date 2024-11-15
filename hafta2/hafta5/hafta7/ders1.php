e18339932a9cc2257c339e53f459c69956c6527c
nisa karagöz <Nisa.karagoz@ogr.gelisim.edu.tr>
2024-11-15 11:39:52 +0300

    Add files via upload

---
+++ hafta2/hafta5/hafta7/ders1.php
@@ -0,0 +1,42 @@
+<?php
+
+//karesini alma
+
+/*for ($i=1;$i<=10; $i++)
+ { 
+    echo $i." sayısının karei:
+	"$i*$i."<br>;
+	
+	 echo $i." sayısının karei:
+	"($i**2)."<br>;
+	echo $i." sayısının karesi:
+	".pow($i,2)."<br>"; 
+} */
+ 
+ 
+//isim yazdırma
+ 
+ /*for ($i=1;$i<=7;$i++)
+ {
+	 echo "<font size=".$i."<br>NİSA KARAGÖZ</br>"."<br>"."</font>"; 
+ } */
+  ?>
+ 
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
