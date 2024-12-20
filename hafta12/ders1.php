49d75727fbe5d28c878268e3575328e6849f0b6b
nisa karagöz <Nisa.karagoz@ogr.gelisim.edu.tr>
2024-12-20 11:31:13 +0300

    Add files via upload

--- hafta12/ders1.php
+++ hafta12/ders1.php
@@ -1,15 +1,102 @@
+<html>
+<head> 
+  <meta charset="UTF-8">
+  <title>EKLE SİL GÜNCELLEME</title>
+ </head>
+ <body>
+   <form name="form" action=""method="post">
+    <table>
+	<tr>
+	  <td>TC NUMARASI:</td>
+	  <td><input type="text" name="tc"></td>
+	  </tr>
+	  <tr>
+	  <td>ADI:</td>
+	  <td><input type="text" name="ad"></td>
+	  </tr>
+	  <tr>
+	  <td>SOYADI:</td>
+	  <td><input type="text" name="soyad"></td>
+	  </tr>
+	  <tr>
+	     <td colspan="3">
+		       <input type="submit" name="kaydet" value="KAYDET">
+			   <input type="submit" name="guncelle" value="GÜNCEL">
+			   <input type="submit" name="sil" value="SİL">
+		</td>
+		</tr>
+		</table>
+		</form>
+		</body>
+</html>
 <?php
-try
+$db=new PDO("mysql:host=localhost; dbname=örnek; charset=utf8","root","");
+if(isset ($_POST["kaydet"]))
 {
-$db=new PDO("mysql:host=localhost; dbname=veritabanı;charset=utf8","root","");
-echo "Kayıt Başarılı";
+	$ktc=$_POST["tc"];
+	$kad=$_POST["ad"];
+	$ksoyad=$_POST["soyad"];
+	if($db)
+	{
+		echo "VERİTABANI BAĞLANTISI GERÇEKLEŞTİ"."
+		<br>";
+		$kaydet=$db->exec("INSERT INTO ögrencidurum(ad,soyad,tc)VALUES
+		('$ktc','$kad','$ksoyad')");
+		if($kaydet)
+		{
+			echo "Kayıt ekleme başarılı";
+		}
+		else 
+		{
+			echo "Kayıt ekleme başarısız";
+		}
+	}
+}	
+   if(isset($_POST["guncelle"]))
+   {
+	$ktc=$_POST["tc"];
+	$kad=$_POST["ad"];
+	$ksoyad=$_POST["soyad"];
+	if($db)
+	{
+		echo "Veri tabanı bağlantısı gerçekleşti!"."
+		<br>";
+		$guncelle=$db->exec("UPDATE ögrencidurum SET ad='$kad',soyad='$ksoyad' WHERE tc='$ktc'");
+		if($guncelle)
+		{
+			echo "Güncelleme başarı ile tamamlanı.";
+		}
+		else
+		{ 
+	       echo "Güncelleme tamamlanamadı.";
+		}
+	}
+   }
+
+    if(isset($_POST["sil"]))
+    {
+	   $ktc=$_POST["tc"];
+	   $kad=$_POST["ad"];
+	   $ksoyad=$_POST["soyad"];
+	if($db)
+    { 
+       echo "Veri tabanı bağlantısı gerçekleşti!"."
+	   <br>";
+	   $sil=$db->exec("DELETE FROM ögrencidurum  WHERE ad='$kad'");
+	   if($sil)
+	   {
+		   echo "Silme işlemi başarı ile tamamlandı.";
+	   }
+	   else
+	   {
+		   echo "Silme işlemi başarısız.";
+	   }
+	   
+	}
+	}
 
 
-}
-catch(PDOException $e)
-{
-	 echo $e->getMassage();
-}
+		?>
 
 
 
@@ -19,11 +106,3 @@ catch(PDOException $e)
 
 
 
-
-
-
-
-
-
-
-?>
