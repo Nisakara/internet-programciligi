e81eae8c7fbda087b6800e07b64057fd916a7145
nisa karagöz <Nisa.karagoz@ogr.gelisim.edu.tr>
2025-01-08 22:13:54 +0300

    Delete çokboyutludizi.php

--- çokboyutludizi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ogrenci=array();
-$ogrenci[0]=array("Çisem","Balıkesir");
-$ogrenci[1]=array("Ahmet","Bursa");
-$ogrenci[2]=array("Banu","Van");
-echo "<pre>";
-print_r($ogrenci);
-echo "</pre>";
-?>
