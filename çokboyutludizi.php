f2eb4c0a6afe7290d6c313d8f0bcbe7d5eb00963
nisa karagöz <Nisa.karagoz@ogr.gelisim.edu.tr>
2025-01-08 22:15:07 +0300

    Add files via upload

---
+++ çokboyutludizi.php
@@ -0,0 +1,9 @@
+<?php
+$ogrenci=array();
+$ogrenci[0]=array("Nisa","kastamonu");
+$ogrenci[1]=array("Berat","malatya");
+$ogrenci[2]=array("Banu","Van");
+echo "<pre>";
+print_r($ogrenci);
+echo "</pre>";
+?>
